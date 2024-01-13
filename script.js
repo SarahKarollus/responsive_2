e4c64b4cb97889f1857f20a417463765e174725b
SarahKarollus <92145369+SarahKarollus@users.noreply.github.com>
2024-01-13 19:38:14 +0100

    section 4 Responsive, Modal hinzugefügt
    passt grob, aber die mobile variante für die Entwicklertools muss noch gemacht werden

--- script.js
+++ script.js
@@ -114,3 +114,32 @@ document.addEventListener("click", function(event) {
         form.style.display = "none";
     }
 });
+
+
+
+
+// Holt das Modal
+var modal = document.getElementById("myModal");
+
+// Holt den Button, der das Modal öffnet
+var btn = document.getElementById("readMore4");
+
+// Holt das <span> Element, das das Modal schließt
+var span = document.getElementsByClassName("close")[0];
+
+// Wenn der Benutzer auf den Button klickt, öffnet sich das Modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Wenn der Benutzer auf <span> (x) klickt, schließt sich das Modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Wenn der Benutzer irgendwo außerhalb des Modals klickt, schließt sich dieses
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
